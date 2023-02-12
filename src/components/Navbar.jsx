1d7588364c1a93d3da45612d7a45874de9264c3e
Ilham Ramadhan <ilhamputra0601@gmail.com>
2023-02-12 10:18:22 +0700

    fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,59 +1,75 @@
 const Navbar = () =>{
-    return(
-        <> 
+    return (
+      <>
         <header class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
-        <div class="container">
+          <div class="container">
             <div class="flex item-center justify-between relative">
-                <div class="px-4">
-                    <a href="#home" class="font-bold text-lg text-primary block py-6">Hamz</a>
-                </div>
-                <div class="flex items-center px-4">
-                    <button id="hamburger" type="button" class="block absolute right-4 lg:hidden  ">
-                        <span class="hamburger-line origin-top-left transition duration-300 ease-in-out"></span> 
-                        <span class="hamburger-line transition duration-300 ease-in-out"></span> 
-                        <span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span> 
-                    </button>
-                    <nav id="nav-menu" class="hidden absolute py-5 bg-white lg:border-transparent  border border-slate-300 shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:max-w-full lg:bg-transparent lg:shadow-none lg:rounded-none dark:text-white  dark:bg-slate-900  lg:dark:bg-transparent ">
-                        <ul class="block lg:flex">
-                            <li class="group">
-                                <a href="#home" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">Beranda</a>
-                            </li>
-                            <li class="group">
-                                <a href="#about" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">About</a>
-                            </li>
-                            <li class="group">
-                                <a href="#portfolio" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">Portfolio</a>
-                            </li>
-                            <li class="group">
-                                <a href="#clients" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">Clients</a>
-                            </li>
-                            <li class="group">
-                                <a href="#blog" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">Blog</a>
-                            </li>
-                            <li class="group">
-                                <a href="#contact" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">Contact</a>
-                            </li>
-                                <hr class="w-48 h-0.5 lg:w-0.5 lg:h-7 mx-auto lg:ml-1 my-4 lg:my-auto bg-slate-600 border-0 rounded lg:inline-block  lg:dark:bg-white "/>
-                            <li class="flex lg:items-center pl-8 mx-auto  ">
-                                <div id="dark" class="flex cursor-pointer ml-7 lg:ml-0 lg:mt-1">
-                                    <span class="mr-2 text-md text-black dark:text-white">Mode </span>
-                                    <span id="terang" class="flex">
-                                        <img  src="public\svg\idea.webp" alt="#" width="30"/>
-                                        <span class="ml-2 text-md text-slate-300 ">light</span>
-                                    </span>
-                                    <span id="gelap" class="hidden flex">
-                                        <img  src="public\svg\night-mode.webp" alt="#" width="30"/>
-                                        <span class="ml-2 text-md text-slate-700">dark</span>
-                                    </span>
-                                </div>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+              <div class="px-4">
+                <a href="#home" class="font-bold text-lg text-primary block py-6">
+                  Hamz
+                </a>
+              </div>
+              <div class="flex items-center px-4">
+                <button id="hamburger" type="button" class="block absolute right-4 lg:hidden  ">
+                  <span class="hamburger-line origin-top-left transition duration-300 ease-in-out"></span>
+                  <span class="hamburger-line transition duration-300 ease-in-out"></span>
+                  <span class="hamburger-line origin-bottom-left transition duration-300 ease-in-out"></span>
+                </button>
+                <nav
+                  id="nav-menu"
+                  class="hidden absolute py-5 bg-white lg:border-transparent  border border-slate-300 shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:max-w-full lg:bg-transparent lg:shadow-none lg:rounded-none dark:text-white  dark:bg-slate-900  lg:dark:bg-transparent ">
+                  <ul class="block lg:flex">
+                    <li class="group">
+                      <a href="#home" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        Beranda
+                      </a>
+                    </li>
+                    <li class="group">
+                      <a href="#about" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        About
+                      </a>
+                    </li>
+                    <li class="group">
+                      <a href="#portfolio" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        Portfolio
+                      </a>
+                    </li>
+                    <li class="group">
+                      <a href="#clients" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        Clients
+                      </a>
+                    </li>
+                    <li class="group">
+                      <a href="#blog" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        Blog
+                      </a>
+                    </li>
+                    <li class="group">
+                      <a href="#contact" class="text-base text-dark py-2 mx-8 group-hover:text-primary2 flex">
+                        Contact
+                      </a>
+                    </li>
+                    <hr class="w-48 h-0.5 lg:w-0.5 lg:h-7 mx-auto lg:ml-1 my-4 lg:my-auto bg-slate-600 border-0 rounded lg:inline-block  lg:dark:bg-white " />
+                    <li class="flex lg:items-center pl-8 mx-auto  ">
+                      <div id="dark" class="flex cursor-pointer ml-7 lg:ml-0 lg:mt-1">
+                        <span class="mr-2 text-md text-black dark:text-white">Mode </span>
+                        <span id="terang" class="flex">
+                          <img src="svg/idea.webp" alt="#" width="30" />
+                          <span class="ml-2 text-md text-slate-300 ">light</span>
+                        </span>
+                        <span id="gelap" class="hidden flex">
+                          <img src="svg/night-mode.webp" alt="#" width="30" />
+                          <span class="ml-2 text-md text-slate-700">dark</span>
+                        </span>
+                      </div>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
             </div>
-        </div>
-    </header>
-        </>
+          </div>
+        </header>
+      </>
     );
 }
 
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () =>{
                 </div>
                 <div class="w-full self-end px-4 lg:w-1/2">
                     <div class="mt-10 relative lg:mt-9 lg:right-0 ">
-                        <img src="public\img\hamz.png" alt="hamz" class="max-w-full mx-auto "/>
+                        <img src="img/hamz.png" alt="hamz" class="max-w-full mx-auto "/>
                         <span class="absolute bottom-0 -z-10 left-1/2 -translate-x-1/2 scale-105 md:scale-125 lg:scale-150"><svg width="400" height="400" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
                             <path fill="#ec4899" d="M39,-39C52.9,-25.1,68.1,-12.5,71.9,3.8C75.7,20.1,68,40.2,54.1,48.6C40.2,57,20.1,53.7,-1.4,55.1C-22.8,56.5,-45.7,62.5,-62.1,54.1C-78.5,45.7,-88.6,22.8,-87.4,1.2C-86.2,-20.5,-73.9,-41,-57.5,-55C-41,-68.9,-20.5,-76.3,-4,-72.3C12.5,-68.3,25.1,-53,39,-39Z" transform="translate(100 100)" />
                         </svg></span>
